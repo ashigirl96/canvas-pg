bd56a6ed900caa094ea6166c934b3cc450e3006b
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2022-08-04 03:30:36 +0900

    add "no-unused-vars": "off", "@typescript-eslint/no-unused-vars": ["error"]

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
     'no-console': 'off',
     'no-undef': 'off',
     'react/jsx-uses-vars': 1,
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
   },
   parser: '@typescript-eslint/parser',
 }
