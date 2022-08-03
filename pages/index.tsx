b596508c7bc3b9f45bebc7bc1b23e904a1df02a1
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2022-08-04 02:36:44 +0900

    downgrade react

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from 'next'
+import React from 'react'
 
+type ContainerProps = {
+  children: React.ReactNode
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div className="flex h-full w-full flex-col select-none">{children}</div>
+  )
+}
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <Container>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Container>
+  )
 }
 
 export default Home
